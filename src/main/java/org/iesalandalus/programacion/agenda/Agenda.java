df8e1e373252165b23f102c20ce75614cfb3cd6e
victoriamarin <victoriamarin@172.20.37.225>
2018-12-02 19:19:11 +0100

    Creamos la clase Agenda y añadimos los atributos numContactos y MAX_CONTACTOS.

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.agenda;
+
+/**
+ *
+ * @author victoriamarin
+ */
+public class Agenda {
+    /**
+     * Hemos añadido el atributo numContactos a la clase Agenda.
+     */
+    private int numContactos;
+    
+    /**
+     * Hemos añadido el atributo MAX_CONTACTO a la clase Agenda.
+     */
+    private static final int MAX_CONTACTOS = 0;
+    
+}
