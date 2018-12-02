7adc476e4f7a6c50ed37c31e73df3b6a559384b1
victoriamarin <victoriamarin@172.20.37.225>
2018-12-02 16:33:38 +0100

    Hemos añadido los atributos a la clase Contacto, incluyendo las ER.

--- src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -7,13 +7,35 @@ package org.iesalandalus.programacion.agenda;
 
 
  /**
- *Hemos añadido los atributo a la clase contacto
+ *
  * @author victoriamarin
  */
 
 public class Contacto {
+    /**
+     * Hemos añadido el atributo nombre a la clase contacto.
+     */
     private String nombre;
+    
+    /**
+     * Hemos añadido el atributo teléfono a la clase contacto.
+     */
     private String telefono;
+    
+    /**
+     * Hemos añadido el atributo correo a la clase contacto.
+     */
     private String correo;
+    
+    /**
+     * Expresión regular con la que validamos un teléfono.
+     */
+    private static final String ER_TELEFONO ="";
+    /**
+     * Expresión regular con la que validamos un correo.
+     */
+    private static final String ER_CORREO ="";
+    
 }
 
+    
