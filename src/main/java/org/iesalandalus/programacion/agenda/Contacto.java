96386ee94bc6be30d468c5c39b49714e4b12fee3
victoriamarin <victoriamarin@172.20.37.225>
2018-12-02 12:23:47 +0100

    Hemos añadido los atributos a la clase Contacto

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.agenda;
+
+
+ /**
+ *Hemos añadido los atributo a la clase contacto
+ * @author victoriamarin
+ */
+
+public class Contacto {
+    private String nombre;
+    private String telefono;
+    private String correo;
+}
+
